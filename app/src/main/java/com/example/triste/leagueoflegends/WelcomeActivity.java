9e53bd08724942473b5207749541a2817c830465
xy <1106693417@qq.com>
2019-09-04 13:15:16 +0800

    增加了欢迎界面,让App更加完善

---
+++ app/src/main/java/com/example/triste/leagueoflegends/WelcomeActivity.java
@@ -0,0 +1,26 @@
+package com.example.triste.leagueoflegends;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        getWindow().setFlags(WindowManager.LayoutParams. FLAG_FULLSCREEN ,
+                WindowManager.LayoutParams. FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(WelcomeActivity.this,MainActivity.class));
+                finish();
+            }
+        },3000);
+    }
+}
